7cb080801a90c9d4a3b55d1b16ec49b20caae435
renan04lima <renan04lima@outlook.com>
2022-03-04 08:56:04 -0300

    feat: setup middlewares and express

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,2 +1,7 @@
 import './config/module-alias'
 import 'reflect-metadata'
+import env from '@/main/config/env'
+import app from '@/main/config/app'
+
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
