e445b0c2242124e6bab7b60d2746aa725a8bda6b
renan04lima <renan04lima@outlook.com>
2022-03-04 09:11:25 -0300

    feat: add express route adapter

---
+++ src/main/adapters/express-route.ts
@@ -0,0 +1,11 @@
+
+import { Controller } from '@/application/helpers'
+import { RequestHandler } from 'express'
+
+type Adapter = (controller: Controller) => RequestHandler
+
+export const adaptExpressRoute: Adapter = controller => async (req, res) => {
+  const { statusCode, data } = await controller.handler({ ...req.body })
+  const json = [200, 201, 204].includes(statusCode) ? data : { error: data.message }
+  res.status(statusCode).json(json)
+}
