126796a48a6a4704a0d53d142a47094ed243c2d8
renan04lima <renan04lima@outlook.com>
2022-03-04 09:05:18 -0300

    feat: add CreateSongController factory

---
+++ src/main/factories/application/controllers/create-song-controller.ts
@@ -0,0 +1,7 @@
+import { CreateSongController } from '@/application/controllers'
+import { Controller } from '@/application/helpers'
+import { makePgFavoriteSongsRepo } from '@/main/factories/infra/repos/postgres'
+
+export const makeCreateSongController = (): Controller => {
+  return new CreateSongController(makePgFavoriteSongsRepo())
+}
