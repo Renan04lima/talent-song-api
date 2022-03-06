d8a0e3509cac8a4290fcd3b5cfede19c8862b640
renan04lima <renan04lima@outlook.com>
2022-03-06 18:12:58 -0300

    feat: ensure GET /favorite-songs works correctly

---
+++ src/main/factories/application/controllers/get-songs-controller.ts
@@ -0,0 +1,7 @@
+import { GetSongsController } from '@/application/controllers'
+import { Controller } from '@/application/helpers'
+import { makePgFavoriteSongsRepo } from '@/main/factories/infra/repos/postgres'
+
+export const makeGetSongsController = (): Controller => {
+  return new GetSongsController(makePgFavoriteSongsRepo())
+}
--- src/main/routes/song-routes.ts
+++ src/main/routes/song-routes.ts
@@ -1,9 +1,11 @@
 import { adaptExpressRoute as adapt } from '@/main/adapters'
 import { makeCreateSongController } from '@/main/factories/application/controllers'
+import { makeGetSongsController } from '@/main/factories/application/controllers/get-songs-controller'
 import { auth } from '@/main/middlewares'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/favorite-songs', auth, adapt(makeCreateSongController()))
+  router.get('/favorite-songs', auth, adapt(makeGetSongsController()))
 }
