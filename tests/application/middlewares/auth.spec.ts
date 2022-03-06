f0269c66d3a2c6e0a43265316c6e554bdf512e54
renan04lima <renan04lima@outlook.com>
2022-03-06 12:41:56 -0300

    test: ensure AuthMiddleware calls Authorize with correct input

--- tests/application/middlewares/auth.spec.ts
+++ tests/application/middlewares/auth.spec.ts
@@ -3,9 +3,16 @@ import { AuthMiddleware } from '@/application/middlewares'
 
 describe('AuthMiddleware', () => {
   let sut: AuthMiddleware
+  let authorizeSpy: jest.Mock
+  let authorization: string
+
+  beforeAll(() => {
+    authorization = 'any_authorization_token'
+    authorizeSpy = jest.fn().mockResolvedValue('any_user_id')
+  })
 
   beforeEach(() => {
-    sut = new AuthMiddleware()
+    sut = new AuthMiddleware(authorizeSpy)
   })
 
   test('should return 403 if not have token authorization', async () => {
@@ -14,6 +21,14 @@ describe('AuthMiddleware', () => {
     expect(result).toEqual({
       statusCode: 403,
       data: new ForbiddenError()
+    })
+  })
+
+  test('should call Authorize with correct input', async () => {
+    await sut.handle({ authorization })
+
+    expect(authorizeSpy).toHaveBeenCalledWith({
+      token: authorization
     })
   })
 })
