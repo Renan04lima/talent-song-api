855631c01513ba5fe03fc87a8379dfd4b4995447
renan04lima <renan04lima@outlook.com>
2022-03-06 13:56:04 -0300

    feat: add express middleware adapter

--- src/main/adapters/express-route.ts
+++ src/main/adapters/express-route.ts
@@ -5,7 +5,7 @@ import { RequestHandler } from 'express'
 type Adapter = (controller: Controller) => RequestHandler
 
 export const adaptExpressRoute: Adapter = controller => async (req, res) => {
-  const { statusCode, data } = await controller.handler({ ...req.body })
+  const { statusCode, data } = await controller.handler({ ...req.body, ...req.locals })
   const json = [200, 201, 204].includes(statusCode) ? data : { error: data.message }
   res.status(statusCode).json(json)
 }
