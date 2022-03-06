03c9e1bb25c68f4a8f13d4b6e9cb76ac2916789b
renan04lima <renan04lima@outlook.com>
2022-03-06 16:22:23 -0300

    feat: ensure PgFavoriteSongsRepo.get find all user songs

--- src/infra/repos/postgres/favorite-songs.ts
+++ src/infra/repos/postgres/favorite-songs.ts
@@ -1,13 +1,24 @@
-import { CreateSong } from '@/domain/usecases'
+import { CreateSong, GetSongs } from '@/domain/usecases'
 import { PgSongs } from '@/infra/repos/postgres/entities'
 import { getRepository } from 'typeorm'
 
-export class PgFavoriteSongsRepo implements CreateSong {
+export class PgFavoriteSongsRepo implements CreateSong, GetSongs {
   async create (input: CreateSong.Input): Promise<CreateSong.Output> {
     const repo = getRepository(PgSongs)
     const { album, artist, favoriteId, songName } = await repo.save(input)
     return {
       album, artist, favoriteId, songName
     }
+  }
+
+  async get ({ userId }: GetSongs.Input): Promise<GetSongs.Output> {
+    const repo = getRepository(PgSongs)
+    const songs = await repo.find({
+      select: ['favoriteId', 'album', 'artist', 'songName'],
+      where: [
+        { userId }
+      ]
+    })
+    return songs
   }
 }
