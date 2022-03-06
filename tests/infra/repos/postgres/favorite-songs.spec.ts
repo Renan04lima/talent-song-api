ceb5f8f2d90423ab0b2b16dba5ed23f3ecc4605b
renan04lima <renan04lima@outlook.com>
2022-03-06 17:42:36 -0300

    test: ensure PgFavoriteSongsRepo.get find all user songs by album, artist or songName

--- tests/infra/repos/postgres/favorite-songs.spec.ts
+++ tests/infra/repos/postgres/favorite-songs.spec.ts
@@ -81,5 +81,58 @@ describe('PgFavoriteSongsRepo', () => {
 
       expect(result).toHaveLength(0)
     })
+
+    it('should find all user songs by album, artist or songName', async () => {
+      const user = await pgUserRepo.save({ email: 'any_email', password: 'any_password' })
+      const song = await sut.create({
+        userId: user.id,
+        songName: 'any_songName_1',
+        artist: 'any_artist_1',
+        album: 'any_album_1'
+      })
+      const song2 = await sut.create({
+        userId: user.id,
+        songName: 'any_songName_2',
+        artist: 'any_artist_2',
+        album: 'any_album_2'
+      })
+      const onlyAlbum = await sut.get({
+        userId: user.id,
+        album: song.album
+      })
+      expect(onlyAlbum).toHaveLength(1)
+      expect(onlyAlbum[0].favoriteId).toBe(song.favoriteId)
+
+      const onlyArtist = await sut.get({
+        userId: user.id,
+        artist: song2.artist
+      })
+      expect(onlyArtist).toHaveLength(1)
+      expect(onlyArtist[0].favoriteId).toBe(song2.favoriteId)
+
+      const onlySongName = await sut.get({
+        userId: user.id,
+        songName: song.songName
+      })
+      expect(onlySongName).toHaveLength(1)
+      expect(onlySongName[0].favoriteId).toBe(song.favoriteId)
+
+      const albumAndArtist = await sut.get({
+        userId: user.id,
+        artist: song.artist,
+        album: song.album
+      })
+      expect(albumAndArtist).toHaveLength(1)
+      expect(albumAndArtist[0].favoriteId).toBe(song.favoriteId)
+
+      const allProperties = await sut.get({
+        userId: user.id,
+        artist: song2.artist,
+        album: song2.album,
+        songName: song2.songName
+      })
+      expect(allProperties).toHaveLength(1)
+      expect(allProperties[0].favoriteId).toBe(song2.favoriteId)
+    })
   })
 })
