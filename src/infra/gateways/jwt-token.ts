227a013309f77e232e6f557eabd6f465d1f1364b
renan04lima <renan04lima@outlook.com>
2022-03-07 11:53:47 -0300

    feat: ensure JwtTokenHandler.generate works correctly

--- src/infra/gateways/jwt-token.ts
+++ src/infra/gateways/jwt-token.ts
@@ -1,12 +1,17 @@
-import { TokenValidator } from '@/domain/contracts/gateways'
+import { TokenGenerator, TokenValidator } from '@/domain/contracts/gateways'
 
-import { JwtPayload, verify } from 'jsonwebtoken'
+import { JwtPayload, verify, sign } from 'jsonwebtoken'
 
-export class JwtTokenHandler implements TokenValidator {
+export class JwtTokenHandler implements TokenValidator, TokenGenerator {
   constructor (private readonly secret: string) {}
 
   async validate ({ token }: TokenValidator.Input): Promise<TokenValidator.Output> {
     const payload = verify(token, this.secret) as JwtPayload
     return payload.key
+  }
+
+  async generate ({ expirationInMs, key }: TokenGenerator.Input): Promise<TokenGenerator.Output> {
+    const expirationInSeconds = expirationInMs / 1000
+    return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
   }
 }
