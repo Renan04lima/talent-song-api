256bea6f0246b88a2a206cca861813e67c172643
renan04lima <renan04lima@outlook.com>
2022-03-07 11:53:54 -0300

    test: ensure JwtTokenHandler.generate works correctly

--- tests/infra/gateways/jwt-token.spec.ts
+++ tests/infra/gateways/jwt-token.spec.ts
@@ -57,4 +57,38 @@ describe('JwtTokenHandler', () => {
       await expect(promise).rejects.toThrow()
     })
   })
+
+  describe('generate', () => {
+    let key: string
+    let expirationInMs: number
+    let token: string
+
+    beforeAll(() => {
+      key = 'any_key'
+      expirationInMs = 1000
+      token = 'any_token'
+      fakeJwt.sign.mockImplementation(() => token)
+    })
+
+    it('should call sign with correct input', async () => {
+      await sut.generate({ key, expirationInMs })
+
+      expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return a token', async () => {
+      const generatedToken = await sut.generate({ key, expirationInMs })
+
+      expect(generatedToken).toBe(token)
+    })
+
+    it('should rethrow if sign throws', async () => {
+      fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
+
+      const promise = sut.generate({ key, expirationInMs })
+
+      await expect(promise).rejects.toThrow(new Error('token_error'))
+    })
+  })
 })
